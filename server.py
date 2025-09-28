ebcc6cd8f2d4013421501a56b8d0cab54a6cbda9
threatt-zm <10990825@live.mercer.edu>
2025-09-28 05:43:46 -0400

    updated to adhere to the PEP8 guidelines

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+    importing the necessary module to create a Flask app.
+    Also importing the emotion_detector() method to use within the application
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +9,11 @@ app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def emot_detector():
+    """
+        method that collects the users text input and analyze it for emotion scores.
+        It prints an error message when an input is not given,
+        and a success message based on the input given 
+    """
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
 
@@ -17,11 +26,21 @@ def emot_detector():
 
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-        
-    return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."
+
+    return f"""
+    For the given statement, the system response is 'anger': {anger_score},
+    'disgust': {disgust_score},
+    'fear': {fear_score},
+    'joy': {joy_score}
+    and 'sadness': {sadness_score}.
+    The dominant emotion is {dominant_emotion}.
+    """
 
 @app.route("/")
 def render_index_page():
+    """
+        method to render the index.html page
+    """
     return render_template("index.html")
 
 if __name__ == "__main__":
